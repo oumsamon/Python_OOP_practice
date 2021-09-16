b1fe52730cff42e20cc4d3b0c4b92dd8ee355609
ls <cancel>
2021-09-15 22:04:34 -0500

    codewar Opposites attract

--- codewar.py
+++ codewar.py
@@ -28,21 +28,54 @@
 # https://www.codewars.com/kata/54edbc7200b811e956000556/train/python
 # Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 
-def count_sheeps(sheep):
-    def count_sheeps(sheep):
-    true_count = sum(sheep)
-    return(true_count)
-#   # TODO May the force be with you
-#   pass
+# def count_sheeps(sheep):
+#     def count_sheeps(sheep):
+#     true_count = sum(sheep)
+#     return(true_count)
+# #   # TODO May the force be with you
+# #   pass
 
-array1 = [True,  True,  True,  False,
-          True,  True,  True,  True ,
-          True,  False, True,  False,
-          True,  False, False, True ,
-          True,  True,  True,  True ,
-          False, False, True,  True ];
+# array1 = [True,  True,  True,  False,
+#           True,  True,  True,  True ,
+#           True,  False, True,  False,
+#           True,  False, False, True ,
+#           True,  True,  True,  True ,
+#           False, False, True,  True ];
               
-test.assert_equals(result := count_sheeps(array1), 17, "There are 17 sheeps in total, not %s" % result)
-Test Results:
-Test Passed
-You have passed all of the tests! :)
+# test.assert_equals(result := count_sheeps(array1), 17, "There are 17 sheeps in total, not %s" % result)
+# Test Results:
+# Test Passed
+# You have passed all of the tests! :)
+
+
+
+# https://www.codewars.com/kata/555086d53eac039a2a000083/train/python
+
+# Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+# Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+def lovefunc( flower1, flower2 ):
+    if flower1 % 2 != 0 and flower2 % 2 != 0:
+        return(False)
+    elif flower1 % 2 == 0 and flower2 % 2 == 0:
+        return(False)
+    elif flower1 % 2 == 0 and flower2 % 2 != 0:
+        return(True)
+    else:
+        return(True)
+
+# Test Results:
+# Fixed Tests
+# Basic Test Cases (4 of 4 Assertions)
+# import codewars_test as test
+# from solution import lovefunc
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(lovefunc(1,4), True)
+#         test.assert_equals(lovefunc(2,2), False)
+#         test.assert_equals(lovefunc(0,1), True)
+#         test.assert_equals(lovefunc(0,0), False)
